2bf2003c53f6af6ddb5ab386847e7661b752a8b9
Subhadeep <subhadeepdhang1@gmail.com>
2022-10-16 08:30:40 +0530

    Added Comments

--- 1.LCD_CALCULATOR/src/main.c
+++ 1.LCD_CALCULATOR/src/main.c
@@ -22,8 +22,8 @@
 #include "gpio.h"
 #include "i2c_lcd.h"
 #include "keypad.h"
-
-
+#define RESULT_BUFFER_SIZE 15
+#define REMAINDER_BUFFER_SIZE 15
 
 void SystemClock_Config(void);
 void key_action (char key);
@@ -31,31 +31,44 @@ char get_keys_from_key_val(uint8_t key_val);
 void show_result_to_lcd(int64_t result, int64_t remainder);
 void convert_int_to_string(int64_t num,char *str, int8_t size);
 
+/**
+ * @brief  Main logic
+ * @retval None
+ */
 int main(void)
 {	
 	uint8_t key_val;
 	char key;
 
+	/* Initialize all drivers involved */
 	HAL_Init();
 	SystemClock_Config();
 	MX_GPIO_Init();
 	MX_I2C1_Init();
 	start_i2c_lcd();
 	setup_keypad_gpio();
-	key_val = scan_keypad();
 
+	key_val = scan_keypad();
 
 	while (1){
 
+		/* scan keypad until any button is pressed*/
 		while (key_val == 0){
 			key_val = scan_keypad();
 			HAL_Delay(170);
 		}
 
+		/* get key character from key value*/
 		key = get_keys_from_key_val(key_val);
+
+		/* print key character in 1st row , except it is '='*/
 		if (key != '=')
 			i2c_lcd_data(key);
+
+		/* Perform action according to key press */
 		key_action(key);
+
+		/* Reset key value */
 		key_val = 0;
 	}
 }
@@ -70,6 +83,11 @@ int main(void)
 	--------------
 */
 
+
+/**
+ * @brief Get key from key value
+ * @retval key character
+ */
 char get_keys_from_key_val(uint8_t key_val)
 {
 	switch(key_val){
@@ -115,6 +133,12 @@ char get_keys_from_key_val(uint8_t key_val)
 	}
 }
 
+
+
+/**
+ * @brief Perform proper action acc. to which button is pressed
+ * @retval None
+ */
 void key_action (char key)
 {	
 	static int8_t addition_flag = 0;
@@ -122,11 +146,12 @@ void key_action (char key)
 	static int8_t multiplication_flag = 0;
 	static int8_t division_flag = 0;
 
+	/* Max input 7 digit number, max output 14 digit number*/
 	static int64_t num1 = 0, num2 = 0, result = 0, remainder = 0;
 
+	/* show calculated value to lcd then reset everything for next calculation */
 	if (key == '='){
-
-		// show calculated value to lcd
+		
 		if (addition_flag){
 			result = num1 + num2;
 		}
@@ -137,11 +162,37 @@ void key_action (char key)
 			result = num1 * num2;
 		}
 		if (division_flag){
+
+			/* if second number is zero print err msg, skip division operation & showing result func */
+			if (num2 == 0){
+				i2c_lcd_set_cursor(1,0);
+				i2c_lcd_string("syntax error !");
+				goto RESET_VAR;
+			}
 			result = num1 / num2;
 			remainder = num1 % num2;
 		}
 
 		show_result_to_lcd(result, remainder);
+
+		/*reset flags and values*/
+		RESET_VAR:
+
+		addition_flag = 0;
+		subtraction_flag = 0;
+		multiplication_flag = 0;
+		division_flag = 0;
+		num1 = 0;
+		num2 = 0;
+		result = 0;
+		remainder = 0;
+	}
+
+	/* clear screen , flags and variables , reset cursor */
+	if (key == 'C'){
+
+		i2c_lcd_clear();
+		i2c_lcd_set_cursor(0,0);
 
 		/*reset flags and values*/
 		addition_flag = 0;
@@ -154,11 +205,7 @@ void key_action (char key)
 		remainder = 0;
 	}
 
-	if (key == 'C'){
-		i2c_lcd_clear();
-		i2c_lcd_set_cursor(0,0);
-	}
-
+	/* if any operation button is pressed -> second number , if not pressed -> first number */
 	if (key >= '0' && key <= '9'){
 
 		if (addition_flag || subtraction_flag || multiplication_flag || division_flag){
@@ -168,6 +215,7 @@ void key_action (char key)
 		}
 	}
 
+	/* operation flags to indicate which operation to perform */
 	if (key == '+' || key == '-' || key == 'x' || key == '/'){
 
 		if (key == '+')
@@ -182,44 +230,58 @@ void key_action (char key)
 
 }
 
+
+/**
+ * @brief Showing result to LCD 
+ * @retval None
+ */
 void show_result_to_lcd(int64_t result, int64_t remainder)
 {
 	uint8_t negative_flag = 0;
-	char result_buff[8];
-	char remainder_buff[8];
+	char result_buff[RESULT_BUFFER_SIZE];
+	char remainder_buff[REMAINDER_BUFFER_SIZE];
+
 	i2c_lcd_set_cursor(1,0);
 	i2c_lcd_string("= ");
 
+	/* if result is zero , skip 'convert to int' part */
 	if (result == 0){
 		i2c_lcd_data('0');
 		goto REMAINDER;
 	}
 
+	/* if result is -ve , put '-' sign on lcd & make the number + ve*/
 	(result > 0) ? (negative_flag = 0) : (negative_flag = 1);
 	if (negative_flag){
 		i2c_lcd_data('-');
 		result = -result;
 	}
-	convert_int_to_string(result,result_buff,8);
+	convert_int_to_string(result,result_buff,RESULT_BUFFER_SIZE);
 	i2c_lcd_string(result_buff);
 
 	REMAINDER:
 
+	/* print remainder only when it is non zero*/
 	if (remainder != 0){
-		convert_int_to_string(remainder,remainder_buff,8);
-		i2c_lcd_set_cursor(1,8);
+		convert_int_to_string(remainder,remainder_buff,REMAINDER_BUFFER_SIZE);
+		i2c_lcd_set_cursor(1,10);
 		i2c_lcd_string(remainder_buff);
 	}
 }
 
 
+/**
+ * @brief Int to String conversion
+ * @retval None
+ */
 void convert_int_to_string(int64_t num,char *str, int8_t size)
 {
-
 	int8_t temp, index = 0;
 	
-	for (int8_t i = 0; i < size; i++){str[i] = 0;}
+	/* reset all array elements */
+	for (volatile int8_t i = 0; i < size; i++){str[i] = 0;} 
 
+	/* store number into array in reverse order */
 	while (num){
 		temp = num % 10;
 		str[index] = temp + 48;
@@ -227,7 +289,10 @@ void convert_int_to_string(int64_t num,char *str, int8_t size)
 		index++;
 	}
 
-	for (int8_t i = 0; i < index / 2; i++){
+	/* reverse the array to get correct ordered string of given number */
+	int8_t len = index;
+
+	for (int8_t i = 0; i < len / 2; i++){
 		temp = str[i];
 		str[i] = str[index-1-i];
 		str[index-1-i] = temp;
@@ -280,6 +345,8 @@ void SystemClock_Config(void)
 		Error_Handler();
 	}
 }
+
+
 
 /**
  * @brief  This function is executed in case of error occurrence.
